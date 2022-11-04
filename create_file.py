56f65cac534052fd6fe0e4584d3114e5737ca2a7
Farhad Ahamed Biswas <farhadahamed06@gmail.com>
2022-11-05 00:11:49 +0530

    commit

---
+++ create_file.py
@@ -0,0 +1,7 @@
+'''f=open("Lorem.txt","x")
+f.write("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent semper consectetur purus, quis cursus lorem suscipit vitae. Ut scelerisque aliquet neque, vel hendrerit nisl placerat et. Etiam augue sem, ornare ut maximus quis, rhoncus et arcu. Proin accumsan tincidunt metus, interdum faucibus lorem luctus non. Donec tellus urna, suscipit a augue id, elementum sagittis felis. Fusce et arcu eget nulla ultricies scelerisque. Nunc pulvinar, ligula sed pulvinar pretium, diam mi suscipit ante, in fermentum quam nisi vitae ante. Sed at dui dui. Pellentesque condimentum sed tellus gravida scelerisque. ")
+f.close()'''
+
+f1=open("Lorem.txt","a")
+f1.write("\nVivamus non congue odio. Fusce quis ante erat. Nunc justo sem, fringilla vitae turpis nec, tempus auctor libero. Nunc pulvinar velit at dolor porta, et sollicitudin lectus gravida. Mauris sodales elit nec justo aliquam, sit amet euismod elit pellentesque. In nisi felis, vestibulum eu diam sed, maximus egestas dolor. Suspendisse vulputate felis ipsum, vitae euismod nulla dignissim sit amet. Nam viverra sapien a risus suscipit, quis cursus risus ullamcorper. Morbi vitae rhoncus lacus.")
+f1.close()
---
+++ stack.py
@@ -0,0 +1,18 @@
+import sys
+
+'''while True:
+    print("1.PUSH.2.POP,3,Exit")
+    choice=input("Enter your choice")
+    if choice=='1':
+        print("You input 1")
+    elif choice=='2':
+        print("You choice 2")
+    elif choice=='3':
+        sys.exit("You exit from program")
+'''
+
+
+l=[5,4,3,2,1,0]
+for i in range((len(l)-1),-1,-1):
+    print(l[i])
+
